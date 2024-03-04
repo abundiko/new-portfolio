e835daecb21e206391918d52b93f1ea4c146ca82
Abundance Ken-Dickson C <102532809+abundiko@users.noreply.github.com>
2024-03-04 21:48:04 +0100

    Create cdn.js

---
+++ cdn.js
@@ -0,0 +1,8 @@
+function ______(){
+const footer = document.querySelector('footer');
+  if(!footer) return;
+const me = `<h1 style="height:0;width:0;opacity:0;overflow:hidden;">Abundance Ken-Dickson (abundiko)</h1>`;
+footer.innerHTML += me;
+}
+
+______();
